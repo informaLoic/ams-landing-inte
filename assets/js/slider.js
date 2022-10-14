9efbb53eceb40d820803c7be66f9beb651f31b06
informaLoic <loic@euromedicom.com>
2022-10-14 17:06:27 +0200

    slider ajout fonction click

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,10 +1,47 @@
 const slider = {
-
+    slider: null,
+    buttons: null,
+    lastClickDate: null,
+    
     init: function() {
-        
-        console.log('hello world');
+        console.log('slider.js');
 
+        slider.slider = document.querySelector('.slider-testimonials');
+        slider.buttons = document.querySelectorAll('.testimonials-bullets span');
+
+        for(let button of slider.buttons) {
+            button.addEventListener('click', slider.handleClick);
+        }
+        
+        
+    },
+
+    handleClick: function(e) {
+        e.preventDefault();
+        const currentDate = new Date();
+        slider.lastClickDate = currentDate.getTime();
+
+        const clickedButton = e.currentTarget;
+        let slideToDisplay = clickedButton.dataset.slideNumber;
+
+        slider.displaySlide(slideToDisplay);
+        slider.setCurrentButton(clickedButton);
+    },
+
+    displaySlide: function(slideNumber) {
+        let slideWidth = slider.slider.offsetWidth;
+        let horizontalScroll = slideWidth * slideNumber;
+        slider.slider.style.left = - horizontalScroll + 'px';
+    },
+
+    setCurrentButton: function(clickedButton) {
+        const currentButton = document.querySelector('.testimonials-bullets span.active');
+        currentButton.classList.remove('active');
+        clickedButton.classList.add('active');
+        
     }
+  
+
      
 };
 document.addEventListener('DOMContentLoaded', slider.init);
